152dfa9c67c25be78722e410142ea09021601a44
bigflyfish <1609770902@qq.com>
2021-09-20 21:04:22 +0800

    hot-fix commit first

--- src/main/java/com/bbf/GitTest.java
+++ src/main/java/com/bbf/GitTest.java
@@ -2,8 +2,9 @@ package com.bbf;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
